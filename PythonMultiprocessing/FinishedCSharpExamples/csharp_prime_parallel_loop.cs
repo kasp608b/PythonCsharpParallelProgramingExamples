12c3cc396e9c3f66946493ca2238753d72340a5d
kasp608b <54952896+kasp608b@users.noreply.github.com>
2022-10-18 19:17:07 +0200

    Csharp parallel loop example done

---
+++ PythonMultiprocessing/FinishedCSharpExamples/csharp_prime_parallel_loop.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class PrimeGenerator
+    {
+        public long task(int value)
+        {
+            if (IsPrime(value))
+            {
+                return value;
+
+            } 
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public List<long> GetPrimesSequential()
+        {
+
+            List<long> primes = new List<long>();
+
+            for (int i = 1000000; i <= 2000000; i++)
+            {
+                if (task(i) != 0)
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+
+        }
+
+    public List<long> GetPrimesParallel()
+    {
+
+        ConcurrentQueue<long> PrimesFound = new ConcurrentQueue<long>();
+        Parallel.For(1000000, 2000000, (i) =>
+        {
+            if (IsPrime(i))
+            {
+                PrimesFound.Enqueue(i);
+            }
+        });
+
+        List<long> sortedPrimes = PrimesFound.ToList<long>();
+        sortedPrimes.Sort();
+
+        return sortedPrimes;
+
+    }
+
+    private bool IsPrime(long number)
+        {
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var i = 3;
+            while (i * i <= number)
+            {
+                if(number % i == 0)
+                {
+                    return false;
+                }
+                i += 2;
+            }
+
+            return true;
+            
+        }
+
+    }
+
+
+class Program
+{   
+    static void Main(string[] args)
+    {
+        PrimeGenerator pg = new PrimeGenerator();
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        List<long> primes = pg.GetPrimesSequential();
+
+        sw.Stop();
+
+        string time = String.Format("{0:f3} sec.", sw.Elapsed.TotalSeconds);
+
+        Console.WriteLine(time + " GetPrimesSequential");
+        /*
+        foreach( long prime in primes)
+        {
+            Console.Write(prime + ", ");
+        }
+        */
+
+        Console.WriteLine();
+
+        sw = Stopwatch.StartNew();
+
+        primes = pg.GetPrimesParallel();
+
+        sw.Stop();
+
+         time = String.Format("{0:f3} sec.", sw.Elapsed.TotalSeconds);
+
+        Console.WriteLine(time + " GetPrimesParallel");
+        /*
+        foreach (long prime in primes)
+        {
+            Console.Write(prime + ", ");
+        }
+        */
+
+        Console.WriteLine();
+
+
+    }
+}
+
--- PythonMultiprocessing/FinishedPythonExamples/python_prime_process_pool.py
+++ PythonMultiprocessing/FinishedPythonExamples/python_prime_process_pool.py
@@ -23,18 +23,17 @@ def get_primes_process():
    with Pool() as pool:
        return [x for x in pool.map(task, range(1000000, 2000000)) if x != None]
     
-def is_prime(n):
-    """"pre-condition: n is a nonnegative integer
-    post-condition: return True if n is prime and False otherwise."""
-    if n < 2: 
+def is_prime(number):
+
+    if number < 2: 
          return False;
-    if n % 2 == 0:
-         return n == 2  # return False
-    k = 3
-    while k*k <= n:
-         if n % k == 0:
+    if number % 2 == 0:
+         return number == 2  # return False
+    i = 3
+    while i*i <= number:
+         if number % i == 0:
              return False
-         k += 2
+         i += 2
     return True
 
 # protect the entry point
