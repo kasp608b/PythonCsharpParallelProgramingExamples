1cef674de18772a4e27e324a0f3389d8c9fb7b8d
kasp608b <54952896+kasp608b@users.noreply.github.com>
2022-10-19 22:30:37 +0200

    Threading example work

---
+++ PythonCsharpParallelProgramingExamples/Csharp_threads/csharp_threads.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Diagnostics;
+
+//Example of Threads in C#
+
+void task(int id, int sleepTime)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("Task {0} is running on " + "thread: {1}" + " and running on processor: {2}", id, Thread.CurrentThread.ManagedThreadId, Thread.GetCurrentProcessorId());
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        float number = 2;
+
+        while (true)
+        {
+            number = number * number;
+
+            if(sw.ElapsedMilliseconds >= sleepTime)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Task {0} is finished", id);
+                break;
+            }
+        }
+    }
+}
+
+Thread task1 = new Thread(() => task(1, 3000));
+Thread task2 = new Thread(() => task(2, 3000));
+Thread task3 = new Thread(() => task(3, 4000));
+Thread task4 = new Thread(() => task(4 ,5000));
+// Total 150 seconds
+Stopwatch sw = Stopwatch.StartNew();
+
+task1.Start();
+task2.Start();
+task3.Start();
+task4.Start();
+
+task1.Join();
+task2.Join();
+task3.Join();
+task4.Join();
+
+
+long time = sw.ElapsedMilliseconds;
+Console.WriteLine(time / 1000 + " seconds");
---
+++ PythonCsharpParallelProgramingExamples/FinishedPythonExamples/python_threads.py
@@ -0,0 +1,44 @@
+
+#Example of Threads in Python
+import threading
+import time
+from threading import Thread
+import os
+
+def task(id, sleep_time):
+    for i in range(10):
+        thread_id = threading.currentThread().ident
+        process_id = os.getpid()
+        print(f"Task {id} is running on thread: {thread_id} and running on process: {process_id}")
+        print()
+        
+        number = 2
+        start_time = time.time()
+
+        while True:
+            number = number * number
+            if (time.time() - start_time) < sleep_time:
+                print
+                break;
+
+if __name__ == '__main__':
+    task1 = Thread(target=task, args=(1, 3))
+    task2 = Thread(target=task, args=(2, 3))
+    task3 = Thread(target=task, args=(3, 4))
+    task4 = Thread(target=task, args=(4, 5))
+    # Total 100 seconds
+
+    start_time = time.time()
+
+    task1.start()
+    task2.start()
+    task3.start()
+    task4.start()
+
+    task1.join()
+    task2.join()
+    task3.join()
+    task4.join()
+
+    duration = time.time() - start_time
+    print(f"{duration} seconds")
