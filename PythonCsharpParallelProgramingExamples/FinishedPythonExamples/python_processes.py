eed86ec4d5a729c991a41f0f91d6912f02fd3dcf
kasp608b <54952896+kasp608b@users.noreply.github.com>
2022-10-20 00:21:49 +0200

    Processor example done

---
+++ PythonCsharpParallelProgramingExamples/FinishedPythonExamples/python_processes.py
@@ -0,0 +1,45 @@
+
+#Example of Processes in Python
+import threading
+import time
+import colorama
+from multiprocessing import Process
+import os
+
+def task(id, sleep_time):
+    for i in range(10):
+        thread_id = threading.currentThread().ident
+        process_id = os.getpid()
+        print(f"Task {id} is running on thread: {thread_id} and running on process: {process_id}")
+        print()
+        
+        number = 2
+        start_time = time.time()
+
+        while True:
+            number = number * number
+            if (time.time() - start_time) >= sleep_time:
+                print(colorama.Fore.RED + f"Task {id} is finished:")
+                break;
+
+if __name__ == '__main__':
+    task1 = Process(target=task, args=(1, 3))
+    task2 = Process(target=task, args=(2, 3))
+    task3 = Process(target=task, args=(3, 4))
+    task4 = Process(target=task, args=(4, 5))
+    # Total 150 seconds
+
+    start_time = time.time()
+
+    task1.start()
+    task2.start()
+    task3.start()
+    task4.start()
+
+    task1.join()
+    task2.join()
+    task3.join()
+    task4.join()
+
+    duration = time.time() - start_time
+    print(f"{duration} seconds")
---
+++ PythonCsharpParallelProgramingExamples/csharp_tasks/csharp_tasks.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Diagnostics;
+
+//Example of Tasks in C#
+
+void task(int id, int sleepTime)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        float number = 2;
+
+        while (true)
+        {
+            number = number * number;
+
+            if (sw.ElapsedMilliseconds >= sleepTime)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Task {0} is finished", id);
+                break;
+            }
+        }
+    }
+}
+
+Task task1 = Task.Factory.StartNew(() => task(1, 3000));
+Task task2 = Task.Factory.StartNew(() => task(2, 3000));
+Task task3 = Task.Factory.StartNew(() => task(3, 4000));
+Task task4 = Task.Factory.StartNew(() => task(4, 5000));
+
+// Total 150 seconds
+Stopwatch sw = Stopwatch.StartNew();
+
+Task.WaitAll(task1, task2, task3, task4);
+
+long time = sw.ElapsedMilliseconds;
+Console.WriteLine(time / 1000 + " seconds");
+
